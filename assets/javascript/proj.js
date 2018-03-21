8a5a9764ec6a64fbedda92e289e800c1ab166726
Molly Cougill <molly.cougill@gmail.com>
2018-03-20 20:08:23 -0500

    add placeholder asteroid data chart

--- assets/javascript/proj.js
+++ assets/javascript/proj.js
@@ -17,4 +17,51 @@ $(document).ready(function () {
 
     });
 
+    //build placeholder 
+    var trace1 = {
+        x: [1, 2, 3, 4],
+        y: [12, 11, 12, 13],
+        text: ['A<br>size:40', 'B<br>size:60', 'C<br>size: 80', 'D<br>size: 100'],
+        mode:'markers',
+        marker: {
+            size: [400, 600, 800, 1000],
+            sizemode: 'area'
+        }
+    };
+
+    var trace2 = {
+        x: [1, 2, 3, 4],
+        y: [14, 15, 16, 17],
+        text: ['A<br>size:40</br>sixeref: 0.2', 'B<br>size:60</br>sixeref: 0.2', 'C<br>size: 80</br>sixeref: 0.2', 'D<br>size: 100</br>sixeref: 0.2'],
+        mode:'markers',
+        marker: {
+            size: [400, 600, 800, 1000],
+            sizeref: 2,
+            sizemode: 'area'
+        }
+    };
+
+     var trace3 = {
+        x: [1, 2, 3, 4],
+        y: [20, 21, 22, 23],
+        text: ['A<br>size:40</br>sixeref: 2', 'B<br>size:60</br>sixeref: 2', 'C<br>size: 80</br>sixeref: 2', 'D<br>size: 100</br>sixeref: 2'],
+        mode:'markers',
+        marker: {
+            size: [400, 600, 800, 1000],
+            sizeref: 0.2,
+            sizemode: 'area'
+        }
+    };
+
+    var data = [trace1, trace2, trace3];
+
+    var layout = {
+        title: "THIS IS A PLACEHOLDER CHART",
+        showLegend: true,
+        height: 400,
+        width:480
+    };
+
+    Plotly.newPlot('productTitle', data, layout);
+
 });
