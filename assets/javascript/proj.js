19065d8910f4e45c4c39a8ef1b1d0695ff9d5275
Brant Campodonico <brantcampodonico@Brants-MacBook-Pro.local>
2018-03-20 19:24:59 -0500

    added ajax call for simple user input url

--- assets/javascript/proj.js
+++ assets/javascript/proj.js
@@ -1,1 +1,20 @@
-console.log("hi");
+$(document).ready(function () {
+    var userInput = "T";
+
+    var query = "{'spec':" + userInput + "}";
+
+    var limit = "1";
+
+    var url = "http://asterank.com/api/asterank?query=" + query + "&limit=" + limit;
+
+
+    $.ajax({
+        url: url,
+        method: "GET",
+    }).then(function (response) {
+
+        console.log(response);
+
+    });
+
+});
