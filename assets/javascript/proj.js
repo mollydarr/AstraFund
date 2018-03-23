e5c7edaa2f86c4cef52b8cf3c2b9669e24243513
Brant Campodonico <brantcampodonico@Brants-MacBook-Pro.local>
2018-03-22 21:19:49 -0500

    created userInput variable

--- assets/javascript/proj.js
+++ assets/javascript/proj.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function () {
-    var userInput; //= $("#asteroid-input").val();
+    var userInput = "\'T\'"; //= $("#asteroid-input").val();
 
     var query = '{"spec":' + userInput + '}';
 
