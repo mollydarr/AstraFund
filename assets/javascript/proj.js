186d7deb0102c3a143d2074e27713a43d420d9f8
Brant Campodonico <brantcampodonico@Brants-MBP.fios-router.home>
2018-03-25 23:19:31 -0500

    still cleaning up user invest function

--- assets/javascript/proj.js
+++ assets/javascript/proj.js
@@ -237,7 +237,7 @@ $(document).ready(function () {
     }
 
     function userData(snapshot) {
-        $("#investDash").empty();
+        //$("#investDash").empty();
 
         var userInvestData = snapshot.val();
         console.log(userInvestData);
@@ -258,7 +258,7 @@ $(document).ready(function () {
 
             $("<td scope='col'>").text(userInvestData[k].investAmt).appendTo(newRow);
 
-            $("#investDash").append(newRow);
+            $("#userInvest").append(newRow);
 
         }
     }
