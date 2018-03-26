76800ef3a6e0ad6191ab086815f637d88fd330c2
Brant Campodonico <brantcampodonico@Brants-MBP.fios-router.home>
2018-03-25 23:55:33 -0500

    reverted table back to original version, still needs to be corrected

--- assets/javascript/proj.js
+++ assets/javascript/proj.js
@@ -237,7 +237,7 @@ $(document).ready(function () {
     }
 
     function userData(snapshot) {
-        $("#investDash").empty();
+        //$("#userInvest").empty();
 
         var userInvestData = snapshot.val();
         console.log(userInvestData);
@@ -256,7 +256,7 @@ $(document).ready(function () {
 
             $("<td scope='col'>").text("$"+userInvestData[k].investAmt).appendTo(newRow);
 
-            $("#investDash").append(newRow);
+            $("#userInvest").append(newRow);
 
         }
     }
